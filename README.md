23dec2fbbe087ebeb1e200c97456674f2fe76315
jh34ghu43gu <jh34ghu43gu@gmail.com>
2021-10-09 23:44:30 -0500

    Add class

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Java TF2 Schema To JSON
- A class that converts the tf2 KeyValues schema into a JSON readable one
+ A class (method) that converts the tf2 KeyValues schema into a JSON readable one
---
+++ SchemaHelper.java
@@ -0,0 +1,98 @@
+package files;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.stream.JsonReader;
+
+import ch.qos.logback.classic.Logger;
+
+/**
+ * A class to deal with the tf2 item schema.
+ * @author jh34ghu43gu
+ */
+public class SchemaHelper {
+	
+	private static final Logger log = (Logger) LoggerFactory.getLogger(SchemaHelper.class);
+	private static String TEMP_FILE_NAME = "schemaTemp";
+	
+	/**
+	 * Turn the tf2 schema into a .json readable file.
+	 * @param filename	Schema file name
+	 * @param newFileName	The output json file name including .json extension
+	 */
+	public static void fixSchema(String filename, String newFileName) {
+		log.debug("Attempting to fix schema file " + filename);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		try {
+			JsonReader jsonReader = new JsonReader(new FileReader(filename));
+			JsonPrimitive obj = gson.fromJson(jsonReader, JsonPrimitive.class);
+			jsonReader.close();
+			log.debug(obj.toString());
+			log.debug((String) obj.getAsString().subSequence(0, 20));
+			log.debug("Can already read schema.");
+		} catch(Exception e) {
+			log.debug(e.getMessage());
+			log.debug("Could not read schema, applying fix...");
+			File temp = null;
+			try {
+				//I hate this very much
+				
+				String old = new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
+				old = old.replaceAll("\\\\", "/");
+				String noTab ="{\n" + old.replaceAll("\t", " ");
+				String noDoubleSpace = noTab.replaceAll("(  +)", " ") + "}";
+				String attributeFix = noDoubleSpace.replaceAll("\" \"", "\":\"");
+				attributeFix = attributeFix.replaceAll("([^:])\"\"", "$1\":\"");
+				//This is because regex hates me and cannot tell if something comes after a newline "\"\n *\\{"
+				String noNewLines = attributeFix.replaceAll(System.getProperty("line.separator"), "NEWLINE");
+				
+				noNewLines = noNewLines.replaceAll(" *NEWLINE( *NEWLINE)+", "NEWLINE");
+				noNewLines = noNewLines.replaceAll(" +NEWLINE", "NEWLINE");
+				String newLineFix = noNewLines.replaceAll("\"NEWLINE *\\{", "\":NEWLINE{");
+				newLineFix = newLineFix.replaceAll("\" *NEWLINE *\"", "\",NEWLINE\"");
+				String closingBracketFix = newLineFix.replaceAll("\\} *NEWLINE *(\"[a-zA-Z0-9_ \\-:\\.]*\":)", "},NEWLINE $1");
+				String revertNEWLINE = closingBracketFix.replaceAll("NEWLINE", "\n");
+				
+				
+				//Convert into a temp file, if anything goes wrong in future schema conversions it can be troubleshooted.
+				temp = File.createTempFile(TEMP_FILE_NAME, ".json");
+				log.debug("Created temp file: " + temp.toString());
+				BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
+				bw.write(revertNEWLINE);
+				bw.close();
+				
+				//Convert back into a pretty json file if it's good.
+				FileReader reader = new FileReader(temp);
+				JsonObject obj = gson.fromJson(reader, JsonObject.class);
+				String s = gson.toJson(obj);
+				FileWriter writer = new FileWriter(newFileName);
+				writer.write(s);
+				writer.flush();
+				writer.close();
+				reader.close();
+				
+				log.debug("Finished converting " + filename + " into the json file: " + newFileName);
+			} catch(Exception e2) {
+				log.error("Something else went wrong applying the fix");
+				log.error(e2.getMessage());
+			} finally {
+				if(!temp.delete()) {
+					log.warn("Failed to delete temp file");
+				}
+			}
+		}
+	}
+
+}
